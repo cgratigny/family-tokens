d4f48125f622a7f8896bdc1214676edf2bc0ac90
Chris Gratigny <cgratigny>
2021-04-26 07:32:57 -0700

    caculate tokens.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -10,5 +10,8 @@ class Activity
 
   classy_enum_attr :token_affect, enum: "TokenAffectOptions", allow_blank: true
 
+  scope :earns, -> { where(token_affect: :earn) }
+  scope :spends, -> { where(token_affect: :spend) }
+
   has_many :time_logs
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -14,12 +14,18 @@ class Kid
 
   has_many :time_logs
 
+  before_save :update_tokens
+
   scope :chronological, -> { order(date_of_birth: :desc)}
 
   def update_tokens!
-    self.tokens_earned = time_logs.where(token_affect: :earn).sum(:tokens).abs
-    self.tokens_spent = time_logs.where(token_affect: :spend).sum(:tokens).abs
-    self.token_balance = (self.initial_token_balance || 0) + self.tokens_earned - self.tokens_spent
+    self.update_tokens
     self.save!
+  end
+
+  def update_tokens
+    self.tokens_earned = time_logs.where(activity: { '$in': Activity.earns.map{ |activity| activity.id.to_s } } ).sum(:tokens)
+    self.tokens_spent = time_logs.where(activity: { '$in': Activity.spends.map{ |activity| activity.id.to_s } } ).sum(:tokens)
+    self.token_balance = (self.initial_token_balance || 0) + self.tokens_earned - self.tokens_spent
   end
 end
