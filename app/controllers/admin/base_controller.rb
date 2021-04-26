15b5fd70261af9453425a87824d32d6e7ca02f20
Chris Gratigny <cgratigny>
2021-04-25 19:53:45 -0700

    disable auth.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,14 +1,14 @@
 class Admin::BaseController < ApplicationController
 
-  if ["alpha", "beta", "production"].include?(Rails.env)
-    before_action :http_auth
-    def http_auth
-      unless params[:controller].match(%r{\Aapi\/v1})
-        authenticate_or_request_with_http_basic do |username, password|
-          username == "parents" && password == "2007"
-        end
-      end
-    end
-  end
+  # if ["alpha", "beta", "production"].include?(Rails.env)
+  #   before_action :http_auth
+  #   def http_auth
+  #     unless params[:controller].match(%r{\Aapi\/v1})
+  #       authenticate_or_request_with_http_basic do |username, password|
+  #         username == "parents" && password == "2007"
+  #       end
+  #     end
+  #   end
+  # end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  if ["alpha", "beta", "production"].include?(Rails.env)
-    before_action :http_auth
-    def http_auth
-      unless params[:controller].match(%r{\Aapi\/v1})
-        authenticate_or_request_with_http_basic do |username, password|
-          username == "gratigny" && password == "2879"
-        end
-      end
-    end
-  end
+  # if ["alpha", "beta", "production"].include?(Rails.env)
+  #   before_action :http_auth
+  #   def http_auth
+  #     unless params[:controller].match(%r{\Aapi\/v1})
+  #       authenticate_or_request_with_http_basic do |username, password|
+  #         username == "gratigny" && password == "2879"
+  #       end
+  #     end
+  #   end
+  # end
 
 end
