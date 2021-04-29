b397b542b81de57e2128edae8a657da306a39c32
Chris Gratigny <cgratigny>
2021-04-29 12:46:42 -0700

    Remove http auth.

--- app/controllers/parents/base_controller.rb
+++ app/controllers/parents/base_controller.rb
@@ -10,15 +10,4 @@ class Parents::BaseController < ApplicationController
     redirect_to [:edit, :parents, :signup] unless current_user.family.signup.complete?
   end
 
-  if ["production"].include?(Rails.env)
-    before_action :http_auth
-    def http_auth
-      unless params[:controller].match(%r{\Aapi\/v1})
-        authenticate_or_request_with_http_basic do |username, password|
-          username == "parents" && password == "2007"
-        end
-      end
-    end
-  end
-
 end
